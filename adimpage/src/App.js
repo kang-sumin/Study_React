000fee91525a9d96078f3795703d5b9780cae2c2
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2023-05-05 22:32:35 +0900

    setting
    setting

---
+++ adimpage/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <p>뭐하지...</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
